c2d044715470bc4e0cf51a63231eb66dca778730
Gaurav Choubisa <gauravchoubisa09@gmail.com>
2025-07-04 16:59:02 +0530

    added search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import Search from "./Search";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { FaCircleUser } from "react-icons/fa6";
+import useMobile from "../hooks/useMobile";
+
 
 const Header = () => {
+
+
+const [ isMobile ] =  useMobile()
+const location = useLocation()
+const isSearchpage = location.pathname === "/search"
+console.log(location)
+console.log(isMobile)
+
   return (
-    <header className="h-20 shadow-md sticky top-0 bg-white z-50">
-      <div className="container mx-auto flex items-center h-full px-2 justify-between">
+    <header className="h-24 flex lg:h-20 lg:shadow-md sticky top-0 bg-white z-50 flex-col justify-center gap-1">
+      {
+        !(isSearchpage && isMobile) && (
+           <div className="container mx-auto flex items-center  px-2 justify-between">
         {/* Logo */}
         <Link to={"/"} className="h-full flex justify-center items-center">
           <img
@@ -26,19 +39,26 @@ const Header = () => {
         </Link>
 
         {/* Search */}
-        <div>
+        <div className="hidden lg:block">
           <Search />
         </div>
 
         {/* Login and Cart */}
-        <div className="flex items-center space-x-4">
-          <button className="text-blue-600 font-medium">Login</button>
-          <button className="relative">
-            <span className="material-icons">shopping_cart</span>
-            <span className="absolute -top-1 -right-2 bg-red-500 text-white text-xs px-1 rounded-full">0</span>
+        <div className="">
+          <button className="text-neutral-600 lg:hidden">
+            <FaCircleUser size={26}/>
           </button>
+        <div className="hidden lg:block ">
+          login and cart
+        </div>
         </div>
       </div>
+        )
+      }
+
+        <div className="container mx-auto px-2 lg:hidden">
+          <Search />
+        </div>
     </header>
   );
 };
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -3,75 +3,66 @@ import { IoSearch } from "react-icons/io5";
 import { useLocation, useNavigate } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
 
-
-
 const Search = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isSearchPage, SetISSearchPage] = useState(false);
-
 
   useEffect(() => {
     const isSearch = location.pathname === "/search";
     SetISSearchPage(isSearch);
   }, [location]);
 
-
   const redirectToSearchPage = () => {
     navigate("/search");
-
   };
-
 
   console.log("search", isSearchPage);
 
-
-
   return (
-    <div className="w-full min-w-[300px] lg:min-w-[420px] h-12 rounded-full border overflow-hidden flex items-center  text-neutral-400 bg-slate-50">
-      <button className="flex justify-center items-center h-full p-3">
+    <div className="w-full min-w-[300px] lg:min-w-[420px] h-10 lg:h-12 rounded-full border overflow-hidden flex items-center  text-neutral-400 bg-slate-50 focus-within:border-primary-200 group">
+      <button className="flex justify-center items-center h-full p-3 group-focus-within:text-primary-200">
         <IoSearch size={22} />
       </button>
-      <div>
-        {
-            !isSearchPage ? (
-                      <div onClick={redirectToSearchPage}>
-        <TypeAnimation
-          sequence={[
-            // Same substring at the start will only be typed out once, initially
-            'Search "milk"',
-            1000, // wait 1s before replacing "Mice" with "Hamsters"
-            'Search "bread"',
-            1000,
-            'Search "vegetables"',
-            1000,
-            'Search "panner"',
-            1000,
-            'Search "chips"',
-            1000, // wait 1s before replacing "Mice" with "Hamsters"
-            'Search "biscuits"',
-            1000,
-            'Search "fruits"',
-            1000,
-            'Search "cheese"',
-            1000,
-          ]}
-          wrapper="span"
-          speed={50}
-          repeat={Infinity}
-        />
+      <div className="w-full">
+        {!isSearchPage ? (
+          <div onClick={redirectToSearchPage}>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'search "milk"',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'search "bread"',
+                1000,
+                'search "vegetables"',
+                1000,
+                'search "panner"',
+                1000,
+                'search "chips"',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'search "biscuits"',
+                1000,
+                'search "fruits"',
+                1000,
+                'search "cheese"',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </div>
+        ) : (
+          <div className="w-full h-full">
+            <input
+              type="text"
+              placeholder="search for aata, daal and more"
+              autoFocus={true}
+              className="bg-transparent w-full h-full outline-none"
+            ></input>
+          </div>
+        )}
       </div>
-            ): (
-                <div className="w-full">
-                    <input
-                    type="text"
-                    placeholder="search for aata, daal and more">
-                    </input>
-                </div>
-            )
-        }
-      </div>
-
     </div>
   );
 };
